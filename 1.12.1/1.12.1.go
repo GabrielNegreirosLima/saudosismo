a5a502c27a257294b99c07d8d9aee3f1a07295e2
gabrielnegreiros <gabrielnegreiroslima@gmail.com>
2019-08-17 16:15:08 -0300

    Add primary exercises and some resolutions in Go :')

---
+++ 1.12.1/1.12.1.go
@@ -0,0 +1,27 @@
+package main
+
+// This exercise reads ages in loop until
+// the last is 0. Than, it does the average
+// of the ages
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var sum, age, i int = 0, 1, 0
+
+	for i = 0; age != 0; i++ {
+		fmt.Println("Hello! Enter your age: ")
+
+		_, err := fmt.Scanf("%d", &age)
+		if err != nil {
+			return
+		}
+
+		sum += age
+	}
+
+	fmt.Printf("Average: %v\n", (sum / (i - 1)))
+}
---
+++ 1.12.1/go.mod
@@ -0,0 +1,3 @@
+module github.com/GabrielNegreirosLima/saudosismo/1.12.1
+
+go 1.12
